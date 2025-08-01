88df0f50250ac8f9eb01ebb3b20c0767ef8b21e1
Mouni S <radhumouni4@gmail.com>
2025-08-01 07:10:49 +0530

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,45 @@
+//if statement
+let age = 20;
+if (age >= 18) {
+  console.log("You are elegible to vote");
+}
+
+
+//if-else statements
+let age = 16;
+if (age >= 18) {
+  console.log("Eligible to vote");
+} else {
+  console.log("Not elegible to vote");
+}
+
+
+//Multiple if-else statements
+let number = 15;
+if (number > 0) {
+  console.log("The number is positive.");
+}
+if (number % 3 === 0) {
+  console.log("The number is divisible by 3.");
+}
+if (number < 100) {
+  console.log("The number is less than 100.");
+}
+
+
+//switch statements
+let marks = 85;
+
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 80) {
+  console.log("Grade B");
+} else if (marks >= 70) {
+  console.log("Grade C");
+} else {
+  console.log("Fail");
+}
+
+
+
+
