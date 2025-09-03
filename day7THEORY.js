f5498381763b00cde33e51003f5cae137ec85eb3
Mouni S <radhumouni4@gmail.com>
2025-09-03 10:25:16 +0530

    Add files via upload

---
+++ day7THEORY.js
@@ -0,0 +1,5 @@
+//import and export modules
+//definition
+//export allows you to make variables,functions or classes availablet to other files
+//import allows you to bring those into other filr
+//used in modular java script
