bf566034419fc8c897c326905c49c0c947262e6a
Mouni S <radhumouni4@gmail.com>
2025-12-30 10:50:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <p  className="bg-blue-200 text-blue-900 p-4 rounded-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sed aperiam enim, nemo adipisci in dolorem autem, mollitia voluptatibus explicabo ratione? Minima facilis quam illo id, unde iure quo optio!</p>
+    </div>
+  )
+}
+
+export default App
